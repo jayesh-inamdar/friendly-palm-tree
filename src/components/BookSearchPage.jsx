66437104971f45dd492a90a3a9decbd018aeffd2
Jayesh_Inamdar <jayeshinamdar316@gmail.com>
2024-06-05 00:20:04 +0530

    second commit

--- src/components/BookSearchPage.jsx
+++ src/components/BookSearchPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import BookCard from './BookCard';
-import { Link } from 'react-router-dom';
 import './BookSearchPage.css'; // Reuse the CSS from BookSearchPage
 
 const BookSearchPage = () => {
--- src/components/BookshelfPage.jsx
+++ src/components/BookshelfPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import BookCard from './BookCard';
-import { Link } from 'react-router-dom';
 
 
 const BookshelfPage = () => {
